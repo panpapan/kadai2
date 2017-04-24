a87fcab3262dd5cf13cf7a03fc6dbb88ee0d5e8e
g15929nm <g15929nm@s30534.tsuda.ac.jp>
2017-04-24 13:24:28 +0900

    文字を入れ替えるメソッドを追加しました。

--- src/com/toy/anagrams/lib/StaticWordLibrary.java
+++ src/com/toy/anagrams/lib/StaticWordLibrary.java
@@ -31,6 +31,8 @@
 
 package com.toy.anagrams.lib;
 
+import java.util.Random;
+
 /**
  * Implementation of the logic for the Anagram Game application.
  */
@@ -85,57 +87,6 @@ final class StaticWordLibrary extends WordLibrary {
         "apple",
         "orange",
         "strawberry"};
-
-    private static final String[] SCRAMBLED_WORD_LIST = {
-        "batsartcoin",
-        "maibuguos",
-        "ratimhteci",
-        "abkclssha",
-        "ibmtpa",
-        "iccrmutsnaec",
-        "ocbmnitaoni",
-        "ocsnqeeutnyl",
-        "ocsnroitmu",
-        "edrcmeneitgn",
-        "edepdnneyc",
-        "idasbmgiauet",
-        "ydanicm",
-        "neacsplutaoni",
-        "qeiuaveltn",
-        "xerpseisno",
-        "aficilatet",
-        "rfgaemtn",
-        "ehaxedicalm",
-        "milpmeneatitno",
-        "niidtsniugsiahleb",
-        "niehiratcen",
-        "nietnret",
-        "ajav",
-        "olacilazitno",
-        "imrcpoorecssro",
-        "anivagitno",
-        "poitimazitno",
-        "aparemert",
-        "aprtcki",
-        "ipkcel",
-        "opylomprich",
-        "irogorsuyl",
-        "isumtlnaoesuyl",
-        "psceficitaoni",
-        "tsurtcreu",
-        "elixalc",
-        "ilekiwse",
-        "amanegemtn",
-        "aminupalet",
-        "amhtmetacsi",
-        "ohjtvaa",
-        "evtrxe",
-        "nuisngde",
-        "rtdatioialn",
-        "papel",
-        "ronega",
-        "rstawrbrye"
-    };
     
     final static WordLibrary DEFAULT = new StaticWordLibrary();
 
@@ -160,7 +111,32 @@ final class StaticWordLibrary extends WordLibrary {
      * @return word at that index in its scrambled form
      */
     public String getScrambledWord(int idx) {
-        return SCRAMBLED_WORD_LIST[idx];
+    	int n = WORD_LIST[idx].length();
+    	String st = WORD_LIST[idx];
+    	
+    	//level引数を追加してもらう。
+    	int level = 2;
+    	
+    	for(int i = 0; i < (level*2); i++) {
+	    	Random r = new Random();
+	    	int ran1 = 0;
+	    	int ran2 = 0;
+	    	while(ran1 == ran2) {
+	    	 ran1 = r.nextInt(n-1);
+	    	 ran2 = r.nextInt(n-1);
+	    	}
+	    	char[] a = new char[n];
+	    	for(int j = 0; j < n; j++) {
+	    	 a[j] = WORD_LIST[idx].charAt(j);
+	    	}
+	    	char tmp = WORD_LIST[idx].charAt(ran1);
+	    	a[ran1] = a[ran2];
+	    	a[ran2] = tmp;
+	    	st = new String(a);
+	    	//デバッグ用です。
+	    	//System.out.println(i+1 + ":" + a[ran1] + "と" + tmp + "を交換したよ");
+    	}
+    	return st;
     }
 
     /**
